63909b422dd8f53405f1d760af4841c1c1ba9b7a
prakash-b16 <67474062+prakash-b16@users.noreply.github.com>
2020-07-04 14:16:27 +0530

    testing
    Testing project

---
+++ DummyLoanSanction-1/src/main/java/com/webo/app/twowheelerloan/loansanction/main/DummyLoanSanction1Application.java
@@ -0,0 +1,13 @@
+package com.webo.app.twowheelerloan.loansanction.main;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DummyLoanSanction1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DummyLoanSanction1Application.class, args);
+	}
+
+}
