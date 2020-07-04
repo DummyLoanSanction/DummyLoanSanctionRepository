a5fe4d92aa1931fb9346076ed01e77698c556948
prakash-b16 <67474062+prakash-b16@users.noreply.github.com>
2020-07-04 14:21:33 +0530

    Update DummyLoanSanction1Application.java
    changed made

--- DummyLoanSanction-1/src/main/java/com/webo/app/twowheelerloan/loansanction/main/DummyLoanSanction1Application.java
+++ DummyLoanSanction-1/src/main/java/com/webo/app/twowheelerloan/loansanction/main/DummyLoanSanction1Application.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DummyLoanSanction1Application {
 
 	public static void main(String[] args) {
+		System.out.println("KKKK");
 		SpringApplication.run(DummyLoanSanction1Application.class, args);
 	}
 
