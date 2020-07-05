736d3761523f2607f489459fd3cc8826212c28cb
shrikantbobade7 <shrikantbobade7@gmail.com>
2020-07-05 16:51:12 +0530

    2nd commit
    2nd commit

---
+++ TwoWheelerLoanSanction/TwoWheelerLoanSanction/src/main/java/com/webo/app/twowheelerloan/loansanction/main/controller/LoanDisbursmentController.java
@@ -0,0 +1,12 @@
+package com.webo.app.twowheelerloan.loansanction.main.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin("*")
+public class LoanDisbursmentController {
+	
+	
+	
+}
---
+++ TwoWheelerLoanSanction/TwoWheelerLoanSanction/src/main/java/com/webo/app/twowheelerloan/loansanction/main/repository/EMIDetailsRepository.java
@@ -0,0 +1,9 @@
+package com.webo.app.twowheelerloan.loansanction.main.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.webo.app.twowheelerloan.loansanction.main.model.EMIDetails;
+
+public interface EMIDetailsRepository extends JpaRepository<EMIDetails, Integer> {
+
+}
